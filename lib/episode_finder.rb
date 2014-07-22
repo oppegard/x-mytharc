062d7df94e314f57326f3593842de145d1dab4c4
Glenn Oppegard <oppegard@gmail.com>
2014-07-22 00:43:56 -0600

    map all the things

--- lib/episode_finder.rb
+++ lib/episode_finder.rb
@@ -1,29 +1,15 @@
 require 'nokogiri'
 class EpisodeFinder
   def self.load_from_xml
-    seasons = Array.new
     xml_string = File.read(File.expand_path('../assets/x-files_episodes.xml', __FILE__))
     xml_doc = Nokogiri::XML(xml_string)
-
-    xml_doc.css('Season').each do |season_xml|
-      season_episodes = XFilesSeason.new
-      season_xml.css('episode').each do |episode_xml|
-        episode = XFilesEpisode.from_xml(episode_xml)
-        season_episodes[episode.epnum] = episode
-      end
-      seasons[season_xml['no'].to_i] = season_episodes.compact
-    end
-    seasons.compact
+    xml_doc.css('Season').map { |season_xml| XFilesSeason.from_xml(season_xml) }
   end
 end
 
 class XFilesSeason < Array
-  def initialize(arr=[])
-    super(arr)
-  end
-
-  def compact
-    XFilesSeason.new(super)
+  def self.from_xml(xml)
+    new(xml.css('episode').map { |episode_xml| XFilesEpisode.from_xml(episode_xml) })
   end
 
   def find_by_title(title)
